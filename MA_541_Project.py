28c3081da9fc7055478311a335dc5dcf2c85097f
Vinoth <63890798+VinothkumarK24@users.noreply.github.com>
2020-10-21 12:12:52 -0400

    Update MA_541_Project.py

--- MA_541_Project.py
+++ MA_541_Project.py
@@ -1,4 +1,3 @@
-
 # Part -1
 import pandas as pd
 import numpy as np
@@ -9,17 +8,16 @@ from scipy.stats import normaltest
 from scipy.stats import chisquare
 from scipy.stats import kstest
 import seaborn as sns
+get_ipython().run_line_magic('matplotlib', 'inline')
 from itertools import combinations
 import matplotlib.pyplot as plt
-%matplotlib inline
 
-df = pd.read_excel('/Users/vinodhkumar/Downloads/ma541.xlsx')
+df = pd.read_excel('/Users/vinodhkumar/Downloads/df.xlsx')
 df.head()
 
 # Finding Mean and Standard deviation for all column
 df.describe()
 df.isnull().sum()
-
 
 # the sample correlations among each pair of the four random variables (columns) of the data.
 corr = df.corr()
@@ -28,7 +26,7 @@ corr_mat
 
 # Heatmap
 ax = sns.heatmap(
-    corr,
+    corr, 
     vmin=-1, vmax=1, center=0,
     cmap=sns.diverging_palette(20, 220, n=200),
     square=True
@@ -41,25 +39,23 @@ ax.set_xticklabels(
 
 
 # Part-2
-
 # 1. A histogram for each column (hint: four histograms total)
 fig, axes = plt.subplots(len(df.columns)//3, 3, figsize=(20, 6))
 for col, axis in zip(df.columns, axes):
     df.hist(column = col, bins = 100, ax=axis)
 
 
-# 2. A time series plot for each column (hint: use the series “1, 2, 3, ..., 1000” as the
+# 2. A time series plot for each column (hint: use the series “1, 2, 3, ..., 1000” as the 
 # horizontal axis; four plots total)
 
 xaxis = np.arange(1,1001)
 plt.plot(xaxis, np.array(df['Close_ETF']))
 plt.show()
-
 y=np.array(df['Close_ETF'])
+
 
 #3. A time series plot for all four columns (hint: one plot including four “curves” and each “curve” describes one column)
 sns.pairplot(df)
-
 
 #4. Three scatter plots to describe the relationships between the ETF column and the OIL
 # column; between the ETF column and the GOLD column; between the ETF column and the JPM column, respectively
@@ -104,15 +100,15 @@ if p > alpha:
 	print('Acc to Kolmogorov-Smirnov test Sample looks Gaussian (fail to reject H0)')
 else:
 	print('Acc to Kolmogorov-Smirnov test Sample does not look Gaussian (reject H0)')
-
-# chisquare
+    
+# chisquare   
 stat, p = chisquare(data)
 print('Statistics=%.3f, p=%.3f' % (stat, p))
 if p > alpha:
 	print('Acc to chisquare test Sample looks Gaussian (fail to reject H0)')
 else:
 	print('Acc to chisquare test Sample does not look Gaussian (reject H0)')
-
+    
 # Normal Test
 stat, p = normaltest(data)
 print('Statistics=%.3f, p=%.3f' % (stat, p))
@@ -120,6 +116,7 @@ if p > alpha:
 	print('Acc to Normal test Sample looks Gaussian (fail to reject H0)')
 else:
 	print('Acc to Normal test Sample does not look Gaussian (reject H0)')
+
 
 # Gaussian distribution checking for Oil column (univariate observations)
 
@@ -145,15 +142,15 @@ if p > alpha:
 	print('Acc to Kolmogorov-Smirnov test Sample looks Gaussian (fail to reject H0)')
 else:
 	print('Acc to Kolmogorov-Smirnov test Sample does not look Gaussian (reject H0)')
-
-# chisquare
+    
+# chisquare   
 stat, p = chisquare(data)
 print('Statistics=%.3f, p=%.3f' % (stat, p))
 if p > alpha:
 	print('Acc to chisquare test Sample looks Gaussian (fail to reject H0)')
 else:
 	print('Acc to chisquare test Sample does not look Gaussian (reject H0)')
-
+    
 # Normal Test
 stat, p = normaltest(data)
 print('Statistics=%.3f, p=%.3f' % (stat, p))
@@ -187,15 +184,15 @@ if p > alpha:
 	print('Acc to Kolmogorov-Smirnov test Sample looks Gaussian (fail to reject H0)')
 else:
 	print('Acc to Kolmogorov-Smirnov test Sample does not look Gaussian (reject H0)')
-
-# chisquare
+    
+# chisquare   
 stat, p = chisquare(data)
 print('Statistics=%.3f, p=%.3f' % (stat, p))
 if p > alpha:
 	print('Acc to chisquare test Sample looks Gaussian (fail to reject H0)')
 else:
 	print('Acc to chisquare test Sample does not look Gaussian (reject H0)')
-
+    
 # Normal Test
 stat, p = normaltest(data)
 print('Statistics=%.3f, p=%.3f' % (stat, p))
@@ -229,15 +226,15 @@ if p > alpha:
 	print('Acc to Kolmogorov-Smirnov test Sample looks Gaussian (fail to reject H0)')
 else:
 	print('Acc to Kolmogorov-Smirnov test Sample does not look Gaussian (reject H0)')
-
-# chisquare
+    
+# chisquare   
 stat, p = chisquare(data)
 print('Statistics=%.3f, p=%.3f' % (stat, p))
 if p > alpha:
 	print('Acc to chisquare test Sample looks Gaussian (fail to reject H0)')
 else:
 	print('Acc to chisquare test Sample does not look Gaussian (reject H0)')
-
+    
 # Normal Test
 stat, p = normaltest(data)
 print('Statistics=%.3f, p=%.3f' % (stat, p))
@@ -245,3 +242,98 @@ if p > alpha:
 	print('Acc to Normal test Sample looks Gaussian (fail to reject H0)')
 else:
 	print('Acc to Normal test Sample does not look Gaussian (reject H0)')
+
+
+# Part 4: Break your data into small groups and let them discuss the importance of the Central Limit Theorem
+
+# Consider the ETF column (1000 values) as the population (x), and do the follows. Any software may be used.
+# Mean for ETF Column
+mean = df.mean(axis = 0, skipna = True) 
+ETF_mean = mean[0]
+print(f'Mean for ETF Column {ETF_mean}')
+
+# Standard deviation for ETF Column
+std = df.std(axis = 0, skipna = True) 
+ETF_std = std[0]
+print(f'Standard deviation for ETF Column {ETF_std}')
+
+
+# # Break the population into 50 groups sequentially and each group includes 20 values.
+seq_sample_means =[]
+for split in np.split(df['Close_ETF'], 50):
+    seq_sample_means.append(split.mean())
+print(seq_sample_means)
+
+
+# Calculate the sample mean (𝑥) of each group. Draw a histogram of all the sample means. Comment on the distribution of these sample means, 
+# i.e., use the histogram to assess the normality of the data consisting of these sample means.
+plt.hist(seq_sample_means, density=True, bins=50)  # `density=False` would make counts
+plt.ylabel('Probability')
+plt.xlabel('Sequntial data split Sample Mean');
+
+
+#Calculate the mean (𝜇𝑥) and the standard deviation (𝜎𝑥) of the data including these sample means. Make a comparison between 𝜇𝑥 and 𝜇𝑥 , between 𝜎𝑥 and 𝜎𝑥 . Here, 𝑛 is
+# √𝑛 the number of sample means calculated from Item 
+
+# To be done
+
+seq_sample_means_10 =[]
+for split in np.split(df['Close_ETF'], 10):
+    seq_sample_means_10.append(split.mean())
+print(seq_sample_means_10)
+plt.hist(seq_sample_means_10, density=True, bins=50)  # `density=False` would make counts
+plt.ylabel('Probability')
+plt.xlabel('Sequntial data split Sample Mean');
+
+#Calculate the mean (𝜇𝑥) and the standard deviation (𝜎𝑥) of the data including these sample means. Make a comparison between 𝜇𝑥 and 𝜇𝑥 , between 𝜎𝑥 and 𝜎𝑥 . Here, 𝑛 is
+# √𝑛 the number of sample means calculated from Item 
+
+# To be done
+
+# Generate 50 simple random samples or groups (with replacement) from the population.
+# The size of each sample is 20, i.e., each group includes 20 values
+
+sample_means =[] 
+for _ in range(50):
+    sample = df['Close_ETF'].sample(20, replace = True)
+    mean = sample.mean()
+    sample_means.append(mean)
+print(sample_means)
+
+# Calculate the sample mean (𝑥) of each group. Draw a histogram of all the sample means. Comment on the distribution of these sample means, i.e., 
+# use the histogram to assess the normality of the data consisting of these sample means.
+
+plt.hist(sample_means, density=True, bins=50)  # `density=False` would make counts
+plt.ylabel('Probability')
+plt.xlabel('Sample Mean');
+
+#Calculate the mean (𝜇𝑥) and the standard deviation (𝜎𝑥) of the data including these sample means. Make a comparison between 𝜇𝑥 and 𝜇𝑥 , between 𝜎𝑥 and 𝜎𝑥 . Here, 𝑛 is
+# √𝑛 the number of sample means calculated from Item 
+
+# To be done
+
+
+# Generate 10 simple random samples or groups (with replacement) from the population.
+# The size of each sample is 100, i.e., each group includes 100 values
+
+sample_means_10 =[] 
+for _ in range(10):
+    sample = df['Close_ETF'].sample(100, replace = True)
+    mean = sample.mean()
+    sample_means_10.append(mean)
+print(sample_means_10)
+
+# # Calculate the sample mean (𝑥) of each group. Draw a histogram of all the sample means. Comment on the distribution of these sample means, i.e., 
+# use the histogram to assess the normality of the data consisting of these sample means.
+
+plt.hist(sample_means_10, density=True, bins=50)  # `density=False` would make counts
+plt.ylabel('Probability')
+plt.xlabel('Sample Mean');
+
+#Calculate the mean (𝜇𝑥) and the standard deviation (𝜎𝑥) of the data including these sample means. Make a comparison between 𝜇𝑥 and 𝜇𝑥 , between 𝜎𝑥 and 𝜎𝑥 . Here, 𝑛 is
+# √𝑛 the number of sample means calculated from Item 
+
+# To be done
+
+# In Part 3 of the project, you have figured out the distribution of the population (the entire
+# ETF column). Does this information have any impact on the distribution of the sample mean(s)? Explain your answer.
